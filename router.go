d9133c82835ae1a62c8364b206e90ad69cc661b9
Xuerrui <1309908827@qq.com>
2023-02-07 20:50:58 +0800

    add feed_vedio

--- router.go
+++ router.go
@@ -13,6 +13,7 @@ func initRouter(r *gin.Engine) {
 
 	// basic apis
 	apiRouter.GET("/feed/", controller.Feed)
+	apiRouter.GET("/feed_vedio/", controller.FeedVedio)
 	apiRouter.GET("/user/", controller.UserInfo)
 	apiRouter.POST("/user/register/", controller.Register)
 	apiRouter.POST("/user/login/", controller.Login)
